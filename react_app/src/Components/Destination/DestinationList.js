38159e6701df5ddc62327ed0ef44d6846659d5bb
Purushotam Rai <purushotamkrrai@gmail.com>
2019-02-12 15:56:41 +0530

    TD-7: Update DestinationList.js and DestinationItem.js to display data.

--- react_app/src/Components/Destination/DestinationList.js
+++ react_app/src/Components/Destination/DestinationList.js
@@ -4,10 +4,17 @@ import DestinationItem from "./DestinationItem";
 export default class DestinationList extends React.Component {
 
   render() {
+    let { data } = this.props;
     return (
       <div>
         <h1>Here are your best Travel Destinations</h1>
-        <DestinationItem/>
+        {data !== null &&
+        data !== undefined &&
+        data.length > 0 ?
+          data.map(item => <DestinationItem {...item} key={item.id}/>)
+          :
+          <div>No destinations found.</div>
+        }
       </div>
     );
   }
